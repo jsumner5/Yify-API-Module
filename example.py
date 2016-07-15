6f8a5462905d1bc4e73e19604bd9bb7f8fe5012c
jeroldsumner@gmail.com <Bigtim21>
2016-07-15 14:47:21 -0400

    Updtaed "example.py"

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
-from Yifi import Yifi;
-#create a instance of YifiApi
+from Yifi import Yifi
+# Create a instance of YifiApi
 YA = Yifi()
-#search yifi with the term "titanic"
-YA.searchMovies("titanic")
-#pass the movie id of titanic to print the url of it
-YA.fetchMovieUrl(3897)
+searchTerm="The Abyss"
+# Search for "The Abyss"
+# assign movies to the returned array
+movies = YA.getSearchMovies(searchTerm)
+# Iterate through movies and print the results
+for movie in movies:
+    print(movie)
+
